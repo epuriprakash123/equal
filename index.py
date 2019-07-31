19e3fdbf191ffbace5ad6d70f454b02c0f307886
epuriprakash123 <48715250+epuriprakash123@users.noreply.github.com>
2019-07-31 16:20:21 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,13 @@
+def ind(x):
+	r=[]
+	for i in range(n):
+		if i==x[i]:
+			r.append(x[i])
+	return r
+n=int(input())
+l=list(map(int,input().split()))
+k=ind(l)
+if len(k)==0:
+	print("'-1'")
+else:
+	print(*k)
